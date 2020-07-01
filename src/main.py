9007c0a6f971de6ddbee2ac9c3ae549fb70c14a4
Carlos Ramos <crono_zs@hotmail.com>
2020-06-30 22:59:01 -0500

    descodificacion del mensaje encontrado en encoded.txt

--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
 # Resolve the problem!!
-
+import re
 
 def run():
-    # Start coding here
+    
+    with open('encoded.txt', 'r', encoding='utf-8') as f:
+        encoded_message = f.read()
+        message = re.findall('[a-z]', encoded_message)
+        print(''.join(message))
 
 
 if __name__ == '__main__':
